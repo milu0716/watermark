9b5aca4a9c9dcf9500d3cfb6bfb314d36366fdf3
milu0716 <tinababy654@gmail.com>
2025-11-21 09:26:41 +0800

    第一次修改測試

--- flutter_application_1/android/app/build.gradle.kts
+++ flutter_application_1/android/app/build.gradle.kts
@@ -24,10 +24,11 @@ android {
         applicationId = "com.example.flutter_application_1"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 24
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        multiDexEnabled = true
     }
 
     buildTypes {
@@ -41,4 +42,13 @@ android {
 
 flutter {
     source = "../.."
+}
+dependencies {
+    // 1. 強制載入 libs 資料夾內的所有檔案
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar", "*.aar"))))
+    
+    // 2. 再次明確指定 (確保萬無一失)
+    implementation(files("libs/ffmpeg-kit-full-gpl-6.0-2.LTS.aar"))
+    implementation(files("libs/smart-exception-java-0.2.1.jar"))
+    implementation(files("libs/smart-exception-common-0.2.1.jar"))
 }
--- flutter_application_1/android/build.gradle.kts
+++ flutter_application_1/android/build.gradle.kts
@@ -1,10 +1,11 @@
 allprojects {
     repositories {
+        
         google()
         mavenCentral()
+        
     }
 }
-
 val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
@@ -14,6 +15,7 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
+    
 }
 subprojects {
     project.evaluationDependsOn(":app")
